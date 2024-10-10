a46891271f8d7ee229cb024b837ae6936619b8e4
Alejandro Varade <Alejandro Varade@DESKTOP-QLSOTS8>
2024-10-10 13:15:33 +0200

    Creación de Oro, atributos clase e instancia. Closes #2

--- ClashofCeuta/src/clashofceuta/ClashofCeuta.java
+++ ClashofCeuta/src/clashofceuta/ClashofCeuta.java
@@ -3,13 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package clashofceuta;
-
+    
 /**
  *
  * @author Alejandro Varade
  */
 public class ClashofCeuta {
-
+    //----------------------------------------
+    // Atributos
+    //----------------------------------------
+    public static final String ICONO = "/img/oro.png";
+    public static final int DURACION = 2 ;
+    public static final int EXPERIENCIA = 1 ;
+    public static final int PRECIO_VENTA = 2 ;
+    public static final int MINAR = 10 ;
+    
+    private String icono ;
+    private int duracionProduccion ;
+    private int experienciaRecolecta ;
+    private int precioVenta ;
+    private int minar ;
     /**
      * @param args the command line arguments
      */
